16b6d7e2f7e7394729daac348e89800dcef512a1
datpham0412 <tiendat041202@gmail.com>
2024-08-24 14:15:47 +1000

    fix: fix typo error in ProductDisplay component

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './ProductDisplay.css'
 import star_icon from '../Assets/star_icon.png'
-import start_dull_icon from '../Assets/star_dull_icon.png'
+import star_dull_icon from '../Assets/star_dull_icon.png'
 
  const ProductDisplay = (props) => {
     const { product } = props;
@@ -20,12 +20,12 @@ import start_dull_icon from '../Assets/star_dull_icon.png'
         </div>
         <div className="productdisplay-right">
             <h1>{product.name}</h1>
-            <div className="productdisplay-right-star">
+            <div className="productdisplay-right-stars">
                 <img src={star_icon} alt="" />
                 <img src={star_icon} alt="" />
                 <img src={star_icon} alt="" />
                 <img src={star_icon} alt="" />
-                <img src={start_dull_icon} alt="" />
+                <img src={star_dull_icon} alt="" />
                 <p>(122)</p>
             </div>
             <div className="productdisplay-right-prices">
@@ -42,15 +42,18 @@ import start_dull_icon from '../Assets/star_dull_icon.png'
             </div>
             <div className="productdisplay-right-size">
                 <h1>Select Size</h1>
-                <div className="productdisplay-right-size">
+                <div className="productdisplay-right-sizes">
                     <div>S</div>
                     <div>M</div>
                     <div>L</div>
                     <div>XL</div>
-                    <div>XXl</div>
-
+                    <div>XXL</div>
                 </div>
             </div>
+            <button>ADD TO CART</button>
+            <p className="productdisplay-right-category"><span>Category:</span> Women, T-Shirt, Crop Top</p>
+            <p className="productdisplay-right-category"><span>Tags:</span> Modern, Latest</p>
+
         </div>
     </div>
   )
